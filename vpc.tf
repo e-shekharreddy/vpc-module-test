093f5b405a818e84dca75eda92ec52415f012689
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-20 13:29:43 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-    source = "git::https://github.com/e-shekharreddy/terraform-aws-vpc.git?ref=main"
+    source = "../terraform-aws-vpc"
     project = var.project
     environment = var.environment
+    is_peering_required = true 
 }
